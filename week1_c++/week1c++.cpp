aedef7dac882d1b6c7d4d63abe09dfd283ae380f
Lance Wiffen <lancewiffen@hotmail.ca>
2017-07-08 15:24:54 -0700

    added request for input, and printing the value of the input number.

--- week1_c++/week1c++.cpp
+++ week1_c++/week1c++.cpp
@@ -14,8 +14,12 @@ int main()
 {
 
 	std::cout << "Hello world\n" << std::endl;
+	std::cout << "enter a number \n";
+	int value;
+	std::cin >> value;
 
-
+	std::cout << "\nThe value of number entered was: " << value;	
+	std::cout << "\n";
 	system("Pause");
 	return 0;
 
