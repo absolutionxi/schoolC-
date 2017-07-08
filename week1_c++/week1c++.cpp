235ae03d7264c8453c3c9dcfad773323aada9fa9
Lance Wiffen <lancewiffen@hotmail.ca>
2017-07-08 15:18:20 -0700

    start of activity

---
+++ week1_c++/week1c++.cpp
@@ -0,0 +1,26 @@
+/*
+
+Write a program to convert the time from 24-hour notation to 12-hour notation and vice versa.
+
+Your program must be menu driven, giving the user the choice of converting the time between the two notations. Furthermore, your program must contain at least the following function:
+a function to convert the time from 24-hour notation to 12-hour notation, a function to convert the time from 12-hour notation to 24-hour notation,
+a function to display the choices, function(s) to get the input, and function(s) to display the results. (For 12-hour time notation, your program must display AM or PM.)
+
+*/
+
+#include <iostream>
+// this is to use just cout instead of cout::std ~~#using namespace std;
+int main()
+{
+
+	std::cout << "Hello world\n" << std::endl;
+
+
+	system("Pause");
+	return 0;
+
+
+
+
+
+}
